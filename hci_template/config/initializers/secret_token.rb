9f694faade71a67bc17d1adcf1f2f4ab4d72dd87
Cristian Pereyra <criis.pereyra@gmail.com>
2011-09-10 17:38:25 -0300

    new_project

---
+++ hci_template/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HciTemplate::Application.config.secret_token = '530db8f4a5069c467260adef2f39dfa30f32d5c78fe192c710c7ac1a05465ac95d549c6b30ba1bc0980e5441ba8212e87aada4ac8d20026b2ce8eac01db75780'
