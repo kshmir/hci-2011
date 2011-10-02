c7be034bdfad7a267572d23c56cc1bd4550e90be
Maximo <maximovs@gmail.com>
2011-10-02 04:18:52 -0300

    update

--- javascripts/qck.application.js
+++ javascripts/qck.application.js
@@ -2,15 +2,16 @@ $.Controller("ApplicationController", {
     init: function(params) {
         this.guide_animation();
         var self = this;
-        $(this.element).find(".login-form input").keypress(function(e) {
+        $(this.element).find(".login-form input").live('keypress', function(e) {
             if (e.which == 13) {
+                alert('sigo aca vieja');
                 $(this).blur();
                 self.login_submit($(this).parent(".login-form:first"));
                 e.preventDefault();
             }
 
         });
-        $(this.element).find(".search-form input").keypress(function(e) {
+        $(this.element).find(".search-form input").live('keypress', function(e) {
             if (e.which == 13) {
                 $(this).blur();
                 self.search_submit($(this).parent(".search-form:first"));
@@ -44,10 +45,10 @@ $.Controller("ApplicationController", {
                 if (currentScroll != lastScroll || currentScroll < guideBottom) {
                     if (!isScrolledIntoView(guide)) {
                         guide.css("position", "fixed")
-                                .css("top", "0")
-                                .css("width", "100%")
-                                .css("z-index", "1000")
-                                .css("background-color", "transparent")
+                            .css("top", "0")
+                            .css("width", "100%")
+                            .css("z-index", "1000")
+                            .css("background-color", "transparent")
                         _onScroll = false;
                     }
                     else {
@@ -73,10 +74,49 @@ $.Controller("ApplicationController", {
             var elemBottom = elemTop + $(elem).height();
 
             return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) // 40: magic number
-                    && (elemBottom <= docViewBottom) && (elemTop >= docViewTop) );
+                && (elemBottom <= docViewBottom) && (elemTop >= docViewTop) );
         }
     }
     ,
+    "history.users.sign_up subscribe" : function(called, data) {
+        // Hides all categories expanded.
+        $(this.element).find("ul ul.subcat").hide("slow");
+        $("#main-content").controller().index();
+        Qck.bread_controller.loadHashes([]);
+    },
+    ".login-form #login_submit click": function(called, data) {
+        $('#sigin_in').click;
+        return false;
+    },
+    "#sign_in click" : function(called, data) {
+
+        $("#sign_in").removeData('qtip')
+            .qtip({
+                content: {
+                    text: $.View("views/login.ejs"),
+                    title: {
+                        text: 'Sign in:',
+                        button: true
+                    }
+                },
+                position: {
+                    my: 'top right', // Use the corner...
+                    at: 'bottom center' // ...and opposite corner
+                },
+                show: {
+                    event: false, // Don't specify a show event...
+                    ready: true, // ... but show the tooltip when ready
+                    effect: function(offset) {
+                        $(this).slideDown(200); // "this" refers to the tooltip
+                    }
+                },
+                hide: false, // Don't specify a hide event either!
+                style: {
+                    classes: 'ui-tooltip-shadow ui-tooltip-' + 'dark'
+                }
+            });
+        return false;
+    },
     change_view: function(selector, ajax) {
         $(selector).fadeOut("slow", function() {
             $(selector).show().html($.View("views/loading.ejs"));
@@ -99,29 +139,29 @@ $.Controller("ApplicationController", {
             };
             var error = function(error_number) {
                 if (error_number) {
-                    $(el).parent(".login:first").fadeIn("slow",function(){
+                    $(el).parent(".login:first").fadeIn("slow", function() {
                         $(".login-form").removeData('qtip')
-                        .qtip({
-                            content: {
-                                text: 'Por favor intente nuevamente.',
-                                title: {
-                                    text: 'Usuario o Contraseña invalidos',
-                                    button: true
+                            .qtip({
+                                content: {
+                                    text: 'Por favor intente nuevamente.',
+                                    title: {
+                                        text: 'Usuario o Contraseña invalidos',
+                                        button: true
+                                    }
+                                },
+                                position: {
+                                    my: 'top right', // Use the corner...
+                                    at: 'bottom center' // ...and opposite corner
+                                },
+                                show: {
+                                    event: false, // Don't specify a show event...
+                                    ready: true // ... but show the tooltip when ready
+                                },
+                                hide: false, // Don't specify a hide event either!
+                                style: {
+                                    classes: 'ui-tooltip-shadow ui-tooltip-' + 'red'
                                 }
-                            },
-                            position: {
-                                my: 'top right', // Use the corner...
-                                at: 'bottom center' // ...and opposite corner
-                            },
-                            show: {
-                                event: false, // Don't specify a show event...
-                                ready: true // ... but show the tooltip when ready
-                            },
-                            hide: false, // Don't specify a hide event either!
-                            style: {
-                                classes: 'ui-tooltip-shadow ui-tooltip-' + 'red'
-                            }
-                        });
+                            });
 
                     });
                 }
@@ -136,8 +176,8 @@ $.Controller("ApplicationController", {
     },
     search_submit: function(el) {
         alert(
-                "HANDLER DEL ENTER :D"
-                );
+            "HANDLER DEL ENTER :D"
+        );
 
     },
     ".login-form input focus": function(el) {
@@ -145,11 +185,11 @@ $.Controller("ApplicationController", {
         if (!(el.attr("id") == "pass")) {
             if (el.attr("id") == "password") {
                 el.hide()
-                        .parent(".login-form:first").find('#pass').show().focus();
+                    .parent(".login-form:first").find('#pass').show().focus();
             }
             if (!el.data("old") || el.data("old") == el.val()) {
                 el.data("old", el.val())
-                        .val("");
+                    .val("");
             }
         }
     },
@@ -168,7 +208,7 @@ $.Controller("ApplicationController", {
         el = $(el);
         if (!el.data("old") || el.data("old") == el.val()) {
             el.data("old", el.val())
-                    .val("");
+                .val("");
         }
     },
     ".search blur": function(el) {
