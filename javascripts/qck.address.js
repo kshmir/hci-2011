e5981cca605fefd582c43c77afff5493164aedd7
Eze <moroco.ortiz@gmail.com>
2011-10-02 11:13:34 -0300

    address con metodos estaticos y dinamicos de Create, Update y GetListAddress

--- javascripts/qck.address.js
+++ javascripts/qck.address.js
@@ -7,21 +7,51 @@ $.Model("Address", {
         //createAddress params:
         //username : is a mandatory param
         //authentication_token : is a mandatory param
-        //this method validates de user and token, and then it instantiates a new .
+        //address: is a mandatory param
+        //this method transform the address param into a XML, and creates a new address
 
         createAddress : function(params, success, error) {
-            params.method = "CreateAddress";
-            $.ajax({
-                            url: Qck.services.order,
-                            data: params,
-                            type : "POST",
-                            success: function(data) {
-
-                                if ($("response", data).attr("status") == "ok") {
-                                    success("OK"); // en caso de crear correctamente el usuario devuelve el string OK
+                params.method = "CreateAddress";
+                params.address = $.View("views/address.ejs", params.address);
+                $.ajax({
+                    url: Qck.services.order,
+                    data: params,
+                    type : "POST",
+                    success: function(data) {
+                           if ($("response", data).attr("status") == "ok") {
+                                    //if the address is correctly create it retrieves the address_id
+                                    success($("address_id", data).text());
                                 }
                                 else {
-                                    error($("error", data).attr("code"));
+                                    error();
+                                }
+                            },
+                             error: error
+
+                        } );
+
+            },
+        //getAddressList method
+        //getAddressList params:
+        //username : is a mandatory param
+        //authentication_token : is a mandatory param
+        //this method retrieves all the address in an XML structure
+
+        getAddressList : function(params, success, error) {
+                params.method = "GetAddressList";
+                $.ajax({
+                    url: Qck.services.order,
+                    data: params,
+
+                    success: function(data) {
+                           if ($("response", data).attr("status") == "ok") {
+                                    //it retrieves de Address List
+                                     var addresslist = [];
+
+                                     success(addresslist);
+                                }
+                                else {
+                                    error();
                                 }
                             },
                              error: error
@@ -37,6 +67,37 @@ $.Model("Address", {
 
     {
 // Instance methods
+
+        //updateAddress method
+        //updateAddress params:
+        //username : is a mandatory param
+        //authentication_token : is a mandatory param
+        //address: is a mandatory param (the new address)
+        //this method transform the address param into a XML, and updates the address
+
+        updateAddress : function(params, success, error) {
+                params.method = "UpdateAddress";
+                params.address = $.View("views/address.ejs", params.address);
+                params.address_id = this.address_id;
+                $.ajax({
+                    url: Qck.services.order,
+                    data: params,
+                    type : "POST",
+                    success: function(data) {
+                           if ($("response", data).attr("status") == "ok") {
+                                    //if the address is correctly create it retrieves and OK message
+                                    success("OK");
+                                }
+                                else {
+                                    error();
+                                }
+                            },
+                             error: error
+
+                        } );
+
+            } ,
+
 
      //Constructor
      setup: function(data) {
